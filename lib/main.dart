cad2ebe3fcdc80c8d35e8b467b5fb803f0bc26d2
Alif-Fikri <aliefmaulanafikri@gmail.com>
2024-06-19 18:39:22 +0700

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: SplashScreen(),
       theme: ThemeData(
         fontFamily: 'Urbanist',
       ),
